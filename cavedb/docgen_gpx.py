d2a604a2c8def36f83e33a1bf0f536fffd259210
Brian Masney <masneyb@onstation.org>
2016-12-23 15:15:14 -0500

    migrated GPX support from XSLT to python

---
+++ cavedb/docgen_gpx.py
@@ -0,0 +1,58 @@
+# Copyright 2016 Brian Masney <masneyb@onstation.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import os.path
+import cavedb.docgen_common
+
+class Gpx(cavedb.docgen_common.Common):
+    def __init__(self, basedir, bulletin):
+        cavedb.docgen_common.Common.__init__(self, basedir, bulletin)
+
+
+    def open(self, all_regions_gis_hash):
+        self.create_directory('/output/gpx')
+        filename = '%s/output/gpx/bulletin_%s.gpx' % (self.basedir, self.bulletin.id)
+        self.gpxfile = open(filename, 'w')
+
+        self.gpxfile.write('<?xml version="1.0" encoding="US-ASCII"?>\n')
+        self.gpxfile.write('<gpx xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' + \
+                           'xmlns="http://www.topografix.com/GPX/1/1" version="1.1" ' + \
+                           'xsi:schemaLocation="http://www.topografix.com/GPX/1/1 ' + \
+                           'http://www.topografix.com/GPX/1/1/gpx.xsd">\n')
+        self.gpxfile.write('  <metadata>\n')
+        self.gpxfile.write('    <name>%s</name>\n' % (self.bulletin.bulletin_name))
+        self.gpxfile.write('  </metadata>\n')
+
+
+    def feature_entrance(self, feature, ent, utmzone, nad27_utmeast, nad27_utmnorth, wgs84_lat, \
+                         wgs84_lon):
+        if (ent.entrance_name):
+            name = '%s - %s' % (feature.name, ent.entrance_name)
+        else:
+            name = feature.name
+
+        self.gpxfile.write('  <wpt lat="%s" lon="%s">\n' % (wgs84_lat, wgs84_lon))
+        self.gpxfile.write('    <ele>%s</ele>\n' % (ent.elevation_ft))
+        self.gpxfile.write('    <name>%s</name>\n' % (name))
+        self.gpxfile.write('    <cmt>%s</cmt>\n' % (feature.feature_type))
+        self.gpxfile.write('    <desc>%s</desc>\n' % (name))
+        self.gpxfile.write('    <sym>Waypoint</sym>\n')
+        self.gpxfile.write('  </wpt>\n')
+
+
+    def close(self):
+        self.gpxfile.write('</gpx>\n')
+        self.gpxfile.close()
+
--- cavedb/generate_docs.py
+++ cavedb/generate_docs.py
@@ -24,12 +24,14 @@ from django.http import HttpResponseRedirect, Http404
 import cavedb.models
 import cavedb.utils
 import cavedb.docgen_composite
+import cavedb.docgen_gpx
 import cavedb.docgen_mxf
 import cavedb.docgen_xml
 
 def write_bulletin_files(bulletin, basedir):
     outputter = cavedb.docgen_composite.Composite(basedir, bulletin,
-                                                  [cavedb.docgen_mxf.Mxf(basedir, bulletin),
+                                                  [cavedb.docgen_gpx.Gpx(basedir, bulletin),
+                                                   cavedb.docgen_mxf.Mxf(basedir, bulletin),
                                                    cavedb.docgen_xml.Xml(basedir, bulletin)])
 
     all_regions_gis_hash = get_all_regions_gis_hash(bulletin.id)
