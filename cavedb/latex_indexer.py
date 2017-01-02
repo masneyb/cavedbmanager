b0d43baf4a0fae274ed68010631c421e5aa35dcb
Brian Masney <masneyb@onstation.org>
2017-01-02 07:33:09 -0500

    refactored latex indexer so that it now works properly with nested terms. Removed ugly hacks put in place for publishing the Tucker County bulletin.

--- cavedb/latex_indexer.py
+++ cavedb/latex_indexer.py
@@ -1,4 +1,4 @@
-# Copyright 2007-2016 Brian Masney <masneyb@onstation.org>
+# Copyright 2007-2017 Brian Masney <masneyb@onstation.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License")
 # you may not use this file except in compliance with the License.
@@ -12,88 +12,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# FIXME - do the following things:
-# - write unit tests
-# - ensure nested replacements are not performed
+# A not so efficient indexer. This class takes as input a list of strings
+# that will be indexed. At a high level:
+#
+# - Take the input string, and go through the list of indexed terms, from
+#   longest to shortest, and replace each occurance with the MD5 digest of
+#   the message term.
+# - Once this has been done, go through all of the MD5 digests, and replace
+#   each occurance with the proper latex \index{} code.
+#
+# See tests/test_latex_indexer.py for an example.
 
-import re
+import hashlib
 
 class LatexIndexer(object):
     def __init__(self, terms):
-        self.__indexed_terms = [], {}
+
+        self.__sorted_terms = []
+        self.__term_to_digest = {}
+        self.__digest_to_index = {}
 
         for term in terms:
             all_index_terms = term.split(':')
             if len(all_index_terms) == 1:
-                self.__indexed_terms[0].append(term)
-                self.__indexed_terms[1][term] = r'\index{%s}%s' % (term, term)
+                self.__add_term(term, [term])
             else:
-                replacement = ''
-                for index_term in all_index_terms[1:]:
-                    replacement = r'%s\index{%s}' % (replacement, index_term)
+                self.__add_term(all_index_terms[0], all_index_terms[1:])
 
-                self.__indexed_terms[0].append(all_index_terms[0])
-                self.__indexed_terms[1][all_index_terms[0]] = '%s%s' % \
-                    (replacement, all_index_terms[0])
-
-        self.__indexed_terms[0].sort(key=lambda term: len(term), reverse=True)
+        self.__sorted_terms.sort(key=lambda term: len(term), reverse=True)
 
 
-    def __clean_index(self, inputstr):
-        # FIXME - ugly hack for Tucker County
-        inputstr = inputstr.replace(r"\caveindex{\caveindex{Great Cave} of \caveindex{Dry Fork} of Cheat River}", \
-                                    r"\caveindex{Great Cave of Dry Fork of Cheat River}")
-        inputstr = inputstr.replace(r"\caveindex{\caveindex{Great Cave} of \caveindex{Dry Fork} of \caveindex{Cheat River}}", \
-                                    r"\caveindex{Great Cave of Dry Fork of Cheat River}")
+    def __add_term(self, search_term, index_terms):
+        self.__sorted_terms.append(search_term)
 
-        flags = re.MULTILINE | re.DOTALL | re.VERBOSE
-        result = re.compile(r'(.*?)(\\caveindex{)(.*?)}(.*)', flags).match(inputstr)
-        if not result:
-            return inputstr
-        elif re.compile(r'^\\caveindex{.*', flags).match(result.group(3)):
-            value = re.sub(r'^\\caveindex{', '', result.group(3))
-            return '%s%s%s%s' % (result.group(1), result.group(2), value, \
-                                 self.__clean_index(result.group(4)))
-        elif not re.compile(r'^.*\\caveindex.*$', flags).match(result.group(3)):
-            if re.compile(r'^.*\\caveindex.*$', flags).match(result.group(4)):
-                return '%s%s%s}%s' % (result.group(1), result.group(2), result.group(3), \
-                                      self.__clean_index(result.group(4)))
-            else:
-                return inputstr
-        else:
-            value = re.sub(r'^(.*?)\s\\caveindex{(.*)', r'\1 \2', result.group(3))
-            return '%s%s' % (result.group(1),
-                             self.__clean_index('%s%s%s' % (result.group(2), value, \
-                                                result.group(4))))
+        search_term_digest = hashlib.md5(search_term).hexdigest()
+        self.__term_to_digest[search_term] = search_term_digest
 
-
-    def __finalize_index(self, inputstr):
-        flags = re.MULTILINE | re.DOTALL | re.VERBOSE
-        result = re.compile(r'(.*?)\\caveindex{(.*?)}(.*)', flags).match(inputstr)
-        if not result:
-            return inputstr
-
-        return '%s%s%s' % (result.group(1), self.__indexed_terms[1][result.group(2)], \
-                           self.__finalize_index(result.group(3)))
+        indexstr = ''
+        for index_term in index_terms:
+            indexstr = r'%s\index{%s}' % (indexstr, index_term)
+        self.__digest_to_index[search_term_digest] = '%s%s' % (indexstr, search_term)
 
 
     def generate_index(self, inputstr):
-        # Add terms to the index
-        for term in self.__indexed_terms[0]:
-            # This appears to be quicker than doing a single regular expression
-            inputstr = inputstr.replace('%s ' % (term), r'\caveindex{%s} ' % (term))
-            inputstr = inputstr.replace('%s.' % (term), r'\caveindex{%s}.' % (term))
-            inputstr = inputstr.replace('%s,' % (term), r'\caveindex{%s},' % (term))
-            inputstr = inputstr.replace('%s:' % (term), r'\caveindex{%s}:' % (term))
-            inputstr = inputstr.replace('%s)' % (term), r'\caveindex{%s})' % (term))
-            inputstr = inputstr.replace('%s\'' % (term), r'\caveindex{%s}\'' % (term))
+        for term in self.__sorted_terms:
+            digest = self.__term_to_digest[term]
 
-        return self.__finalize_index(self.__clean_index(inputstr))
+            # This appears to be quicker than doing a single regular expression
+            for suffix in [' ', '.', ',', ':', ')', '\'']:
+                inputstr = inputstr.replace('%s%s' % (term, suffix),
+                                            '%s%s' % (digest, suffix))
+
+        for digest, replacement in self.__digest_to_index.items():
+            inputstr = inputstr.replace(digest, replacement)
+
+        return inputstr
 
 
     def dump_terms(self, file_handle):
         file_handle.write('\n% Indexed Terms\n')
-        for term in self.__indexed_terms[0]:
-            file_handle.write('%% %s\t%s\n' % (term, self.__indexed_terms[1][term]))
+        for term in self.__sorted_terms:
+            digest = self.__term_to_digest[term]
+            replacement = self.__digest_to_index[digest]
+
+            file_handle.write('%% %s\t%s\t%s\n' % (term, digest, replacement))
         file_handle.write('\n')
 
