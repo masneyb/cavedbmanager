5b0a486ff7531f338af0c4214e93f4de0d856a1b
Brian Masney <masneyb@onstation.org>
2017-01-02 07:04:22 -0500

    updated unit test to show having multiple index entries for a single phrase

--- tests/test_latex_indexer.py
+++ tests/test_latex_indexer.py
@@ -18,21 +18,27 @@ import cavedb.latex_indexer
 class TestLatexIndexer(unittest.TestCase):
     def test_indexer(self):
         terms = ['Great Cave', 'Dry Fork', 'Cheat River', 'Great Cave of Dry Fork of Cheat River', \
-                 'Monongahela River']
+                 'Monongahela River',
+                 'Laurel and Backbone mountains:Laurel Mountain:Backbone Mountain']
         indexer = cavedb.latex_indexer.LatexIndexer(terms)
         indexed = indexer.generate_index('The stream in the Great Cave of Dry Fork of Cheat ' + \
                                          'River empties out into the Dry Fork, which is a ' + \
                                          'tributuary of the Cheat River.\n' + \
                                          '\n' +
                                          'The Cheat River is a tributuary of the Monongahela ' + \
-                                         'River.')
+                                         'River.\n' + \
+                                         '\n' + \
+                                         'Laurel and Backbone mountains are within the county.\n')
         self.assertEqual(indexed, 'The stream in the \\index{Great Cave of Dry Fork of Cheat ' + \
                                   'River}Great Cave of Dry Fork of Cheat River empties out ' + \
                                   'into the \\index{Dry Fork}Dry Fork, which is a tributuary ' + \
                                   'of the \\index{Cheat River}Cheat River.\n' + \
                                   '\n' + \
                                   'The \\index{Cheat River}Cheat River is a tributuary of the ' + \
-                                  '\\index{Monongahela River}Monongahela River.')
+                                  '\\index{Monongahela River}Monongahela River.\n' + \
+                                  '\n' + \
+                                  '\\index{Laurel Mountain}\\index{Backbone Mountain}Laurel ' + \
+                                  'and Backbone mountains are within the county.\n')
 
 if __name__ == '__main__':
     unittest.main()
