8578ebc7e5ce1b6d432745c43a9ea75c5b8d9f26
Brian Masney <masneyb@onstation.org>
2017-01-20 18:32:49 -0500

    reverted last change since it broke wsgi. Ignore the warning from pylint.

--- cavedb/wsgi.py
+++ cavedb/wsgi.py
@@ -16,15 +16,15 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Needed for basic authentication inside Apache
-# https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/apache-auth/
-#pylint: disable=unused-import
-from django.contrib.auth.handlers.modwsgi import check_password
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cavedb.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cavedb.settings")
 sys.path.append('/usr/local/cavedbmanager/cavedb/')
 sys.path.append('/usr/local/cavedbmanager/')
+
+# Needed for basic authentication inside Apache
+# https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/apache-auth/
+#pylint: disable=unused-import,wrong-import-position
+from django.contrib.auth.handlers.modwsgi import check_password
 
 #pylint: disable=invalid-name
 application = get_wsgi_application()
