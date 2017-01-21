1fcc8f3a5ca781433872d976904fcf5d52770bc5
Brian Masney <masneyb@onstation.org>
2017-01-21 05:18:11 -0500

    Now using postgis extension. Project now works correctly inside Docker.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,17 +8,11 @@ psql -lqt root | awk '{print $1}' | grep -qw cavedb
 if [ "$?" != "0" ] ; then
 	cd ../postgis-data-importer
 
-	mkdir download
-	mkdir download/us_wv
-	mkdir download/us_wv/hillshade
-	mkdir download/us_wv/aerial
-	mkdir download/us_wv/aerial/USDA-2014
-	mkdir download/us_wv/aerial/SAMB-2003
-	mkdir download/us_wv/aerial/SAMB-2003/MrSID
-	mkdir download/us_wv/aerial/SAMB-2003/JPG
-	mkdir download/us_wv/aerial/USGS-1994
-	mkdir download/us_wv/dem
-	mkdir download/us_wv/other
+	mkdir -p download/us_wv download/us_wv/hillshade download/us_wv/aerial \
+	         download/us_wv/aerial/USDA-2014 download/us_wv/aerial/SAMB-2003 \
+	         download/us_wv/aerial/SAMB-2003/MrSID \
+	         download/us_wv/aerial/SAMB-2003/JPG download/us_wv/aerial/USGS-1994 \
+	         download/us_wv/dem \ download/us_wv/other
 
 	patch -p1 < ../cavedbmanager/sample-bulletin/postgis-data-importer-sample-bulletin.patch
 
