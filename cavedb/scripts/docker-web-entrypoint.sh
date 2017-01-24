af1e2cb4b655b50086bee295b080ab33e964a618
Brian Masney <masneyb@onstation.org>
2017-01-23 21:12:42 -0500

    added logging when the sample data is populated

--- cavedb/scripts/docker-web-entrypoint.sh
+++ cavedb/scripts/docker-web-entrypoint.sh
@@ -6,7 +6,11 @@ sleep 5
 # Check if the database exists.
 psql -lqt root | awk '{print $1}' | grep -qw "${PGDATABASE}"
 if [ "$?" != "0" ] ; then
+	echo "Populating sample bulletin data..."
+
 	./sample-bulletin/populate-sample-bulletin.sh
+
+	echo "Finised populating sample bulletin data."
 fi
 
 make runRemote
