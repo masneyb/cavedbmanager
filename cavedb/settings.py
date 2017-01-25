6732b6a61d2ea35fd646fb7c46588d6858731717
Brian Masney <masneyb@onstation.org>
2017-01-24 19:04:51 -0500

    use os.environ.get() from python, which does away for the need of get_environment_var(). Add get_boolean() helper. Add options for email configuration for error reporting.

--- cavedb/settings.py
+++ cavedb/settings.py
@@ -15,31 +15,42 @@
 import os
 import cavedb.worker_queue
 
-def get_environment_var(name, default_value):
+def get_boolean(name, defaultValue):
     if name not in os.environ:
-        return default_value
-    return os.environ[name]
+        return defaultValue
+    return os.environ[name] == '1'
 
 
-DEBUG = True if get_environment_var('CAVEDB_DEBUG', '1') == '1' else False
+DEBUG = get_boolean('CAVEDB_DEBUG', True)
+
+# Use EMAIL settings for error reporting if DEBUG is set to False
+# By default, print the emails out to the console. They can be
+# sent out by changing EMAIL_BACKEND to django.core.mail.backends.smtp.EmailBackend
+# and updating the other EMAIL_* settings.
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+EMAIL_USE_TLS = get_boolean('EMAIL_USE_TLS', True)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 INSTALL_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 ADMINS = (
-    (get_environment_var('WEB_ADMIN_FULLNAME', 'Your Name'), \
-     get_environment_var('WEB_ADMIN_EMAIL', 'user@domain.org')),
+    (os.environ.get('WEB_ADMIN_FULLNAME', 'Your Name'), \
+     os.environ.get('WEB_ADMIN_EMAIL', 'user@domain.org')),
 )
 MANAGERS = ADMINS
 
-ALLOWED_HOSTS = get_environment_var('CAVEDB_ALLOWED_HOSTS', 'localhost').split(':')
+ALLOWED_HOSTS = os.environ.get('CAVEDB_ALLOWED_HOSTS', 'localhost').split(':')
 
-ADMIN_SITE_HEADER = get_environment_var('CAVEDB_SITE_HEADER', 'My Cave Database')
+ADMIN_SITE_HEADER = os.environ.get('CAVEDB_SITE_HEADER', 'My Cave Database')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_environment_var('PGDATABASE', 'cavedb'),
+        'NAME': os.environ.get('PGDATABASE', 'cavedb'),
         # By default, get the other database connection parameters from the
         # environment variables PGHOST, PGPORT, PGUSER, PGPASSWORD.
     }
@@ -56,13 +67,13 @@ SITE_ID = 1
 # Specifies where the application is deployed
 CONTEXT_PATH = '/'
 
-MEDIA_ROOT = get_environment_var('CAVEDB_DATA_BASE_DIR', '/usr/local/cavedbmanager-data')
+MEDIA_ROOT = os.environ.get('CAVEDB_DATA_BASE_DIR', '/usr/local/cavedbmanager-data')
 GIS_INCLUDES_DIR = MEDIA_ROOT + '/gis_maps'
 MEDIA_URL = CONTEXT_PATH + 'cavedb/'
 STATIC_URL = '/media/'
 STATIC_ROOT = 'media/'
 
-SECRET_KEY = get_environment_var('CAVEDB_SECRET_KEY', 'FIXME_CHANGE_THIS_SECRET_KEY')
+SECRET_KEY = os.environ.get('CAVEDB_SECRET_KEY', 'FIXME_CHANGE_THIS_SECRET_KEY')
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -115,10 +126,10 @@ INSTALLED_APPS = (
 
 QUEUE_STRATEGY = cavedb.worker_queue.named_pipe_queue
 
-WORKER_FIFO = get_environment_var('CAVEDB_WORKER_FIFO', None)
+WORKER_FIFO = os.environ.get('CAVEDB_WORKER_FIFO', None)
 
 GIS_CONNECTION_TYPE = 'postgis'
-GIS_CONNECTION = 'dbname=%s' % (get_environment_var('CAVEDB_GIS_DBNAME', None))
+GIS_CONNECTION = 'dbname=%s' % (os.environ.get('CAVEDB_GIS_DBNAME', None))
 
 # Contents of fonts.list file for Mapserver
 # Debian-based systems
