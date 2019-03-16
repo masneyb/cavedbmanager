c5301b07f12558c83d8e7474759170b9cd1cce7e
Brian Masney <masneyb@onstation.org>
2019-03-16 11:30:42 -0400

    don't override assertAlmostEqual() to fix pylint warnings. Rename this to assert_tuple_almost_equal and make it handle only tuples.

--- cavedb/tests/test_coord_transform.py
+++ cavedb/tests/test_coord_transform.py
@@ -20,31 +20,31 @@ class TestTransformedCoordinate(unittest.TestCase):
         transformer = TransformedCoordinate('NAD27', 17, 1, 602346, 4388874, None, None)
         self.assertEqual(transformer.get_utm_nad27(),
                          (602346, 4388874))
-        self.assertAlmostEqual(transformer.get_utm_nad83(),
-                               (602362.2488997985, 4389092.512418542))
-        self.assertAlmostEqual(transformer.get_lon_lat_wgs84(),
-                               (-79.80697562078743, 39.64550098182764))
+        self.assert_tuple_almost_equal(transformer.get_utm_nad83(),
+                                       (602362.2488997985, 4389092.512418542))
+        self.assert_tuple_almost_equal(transformer.get_lon_lat_wgs84(),
+                                       (-79.80697562078743, 39.64550098182764))
 
 
     def test_from_utm_nad83(self):
         transformer = TransformedCoordinate('NAD83', 17, 1, 602362, 4389092, None, None)
-        self.assertAlmostEqual(transformer.get_utm_nad27(),
-                               (602345.751114615, 4388873.487573433))
+        self.assert_tuple_almost_equal(transformer.get_utm_nad27(),
+                                       (602345.751114615, 4388873.487573433))
         self.assertEqual(transformer.get_utm_nad83(),
                          (602362, 4389092))
-        self.assertAlmostEqual(transformer.get_lon_lat_wgs84(),
-                               (-79.80697860044248, 39.6454963964772))
+        self.assert_tuple_almost_equal(transformer.get_lon_lat_wgs84(),
+                                       (-79.80697860044248, 39.6454963964772))
 
 
     def test_from_lat_lon_wgs84(self):
         transformer = TransformedCoordinate('WGS84', 17, 1, None, None,
                                             -79.80697562078743, 39.64550098182764)
-        self.assertAlmostEqual(transformer.get_utm_nad27(),
-                               (602346.0000000328, 4388873.999999977))
-        self.assertAlmostEqual(transformer.get_utm_nad83(),
-                               (602362.2489011667, 4389092.512315645))
-        self.assertAlmostEqual(transformer.get_lon_lat_wgs84(),
-                               (-79.80697562078743, 39.64550098182764))
+        self.assert_tuple_almost_equal(transformer.get_utm_nad27(),
+                                       (602346.0000000328, 4388873.999999977))
+        self.assert_tuple_almost_equal(transformer.get_utm_nad83(),
+                                       (602362.2489011667, 4389092.512315645))
+        self.assert_tuple_almost_equal(transformer.get_lon_lat_wgs84(),
+                                       (-79.80697562078743, 39.64550098182764))
 
 
     def test_from_lat_lon_wgs84_no_utm(self):
@@ -52,8 +52,8 @@ class TestTransformedCoordinate(unittest.TestCase):
                                             -79.80697562078743, 39.64550098182764)
         self.assertEqual(transformer.get_utm_nad27(), (None, None))
         self.assertEqual(transformer.get_utm_nad83(), (None, None))
-        self.assertAlmostEqual(transformer.get_lon_lat_wgs84(),
-                               (-79.80697562078743, 39.64550098182764))
+        self.assert_tuple_almost_equal(transformer.get_lon_lat_wgs84(),
+                                       (-79.80697562078743, 39.64550098182764))
 
 
     def test_from_none(self):
@@ -70,16 +70,13 @@ class TestTransformedCoordinate(unittest.TestCase):
         self.assertEqual(transformer.get_lon_lat_wgs84(), (None, None))
 
 
-    def assertAlmostEqual(self, first, second, **kwargs):
-        """Custom `assertAlmostEqual()` which tests the contents of tuples"""
-        if isinstance(first, tuple):
-            if len(first) != len(second):
-                self.fail('First and second tuples are of different lengths (%d vs %d)' %
-                          (len(first), len(second)))
-            for first_, second_ in zip(first, second):
-                super().assertAlmostEqual(first_, second_, places=5, **kwargs)
-        else:
-            super().assertAlmostEqual(first, second, **kwargs)
+    def assert_tuple_almost_equal(self, first, second):
+        if len(first) != len(second):
+            self.fail('First and second tuples are of different lengths (%d vs %d)' %
+                      (len(first), len(second)))
+
+        for first_, second_ in zip(first, second):
+            super().assertAlmostEqual(first_, second_, places=5)
 
 
 if __name__ == '__main__':
