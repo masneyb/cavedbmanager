2d130cdba3ce897c5b3dd52fd3c0909deae478af
Brian Masney <masneyb@onstation.org>
2019-03-16 08:52:22 -0400

    upgrade to ubuntu 18.04

--- cavedb/scripts/docker-db-entrypoint.sh
+++ cavedb/scripts/docker-db-entrypoint.sh
@@ -14,14 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-POSTGRES_BIN=/usr/lib/postgresql/9.6/bin/postgres
+POSTGRES_BIN=/usr/lib/postgresql/10/bin/postgres
 POSTGRES_UID=postgres
 POSTGRES_GID=postgres
 
 chown -R "${POSTGRES_UID}":"${POSTGRES_GID}" "${PGDATA}"
 
 if [ -z "$(ls -A "${PGDATA}")" ]; then
-	gosu "${POSTGRES_UID}" /usr/lib/postgresql/9.6/bin/initdb
+	gosu "${POSTGRES_UID}" /usr/lib/postgresql/10/bin/initdb
 
 	echo "listen_addresses='*'" >> "${PGDATA}"/postgresql.conf
 	echo "host all all 0.0.0.0/0 md5" >> "${PGDATA}"/pg_hba.conf
@@ -33,4 +33,5 @@ if [ -z "$(ls -A "${PGDATA}")" ]; then
 	echo "${SQL}" | gosu "${POSTGRES_UID}" "${POSTGRES_BIN}" --single -E
 fi
 
+chmod 0700 "${PGDATA}"
 exec gosu "${POSTGRES_UID}" "${POSTGRES_BIN}" -D "${PGDATA}"
