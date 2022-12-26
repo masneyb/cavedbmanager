68c156ab585d50a786fecd45f61f801af567f09c
Brian Masney <bmasney@redhat.com>
2022-12-26 02:42:29 -0500

    ensure that directory exists

--- cavedb/scripts/generate_index_txt.py
+++ cavedb/scripts/generate_index_txt.py
@@ -10,9 +10,16 @@ django.setup()
 # pylint: disable=wrong-import-position
 import cavedb.models
 
+def get_directory(name):
+    directory = os.path.join(settings.MEDIA_ROOT, name)
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+    return directory
+
 def generate_bulletins():
-    attachment_filename = os.path.join(settings.MEDIA_ROOT, 'bulletin_attachments', 'index.txt')
-    bulletin_filename = os.path.join(settings.MEDIA_ROOT, 'bulletins', 'index.txt')
+    attachment_filename = os.path.join(get_directory('bulletin_attachments'), 'index.txt')
+    bulletin_filename = os.path.join(get_directory('bulletins'), 'index.txt')
     with open(attachment_filename, 'w', encoding='utf-8') as attachment_index, \
          open(bulletin_filename, 'w', encoding='utf-8') as bulletin_index:
         attachment_index.write('ID\tBulletin Name\r\n')
@@ -25,7 +32,7 @@ def generate_bulletins():
         bulletin_index.write('bulletin_global\tGenerated artifacts for the entire state\r\n')
 
 def generate_features():
-    filename = os.path.join(settings.MEDIA_ROOT, 'feature_attachments', 'index.txt')
+    filename = os.path.join(get_directory('feature_attachments'), 'index.txt')
     with open(filename, 'w', encoding='utf-8') as output:
         output.write('ID\tBulletin\tName\r\n')
 
@@ -35,7 +42,7 @@ def generate_features():
                                              feature.name))
 
 def generate_statewide_docs():
-    filename = os.path.join(settings.MEDIA_ROOT, 'statewide_docs', 'index.txt')
+    filename = os.path.join(get_directory('statewide_docs'), 'index.txt')
     with open(filename, 'w', encoding='utf-8') as output:
         output.write('ID\tDescription\r\n')
 
